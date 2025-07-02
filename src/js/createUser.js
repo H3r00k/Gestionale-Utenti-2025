6f584032e5b7dec8bfb5cb69b1b529ea1a814354
Marco Ienna <marcosk88k@gmail.com>
2025-07-02 23:47:02 +0200

    Aggiunta funzionalità Sfoglia Utenti con espansione dettagli

--- src/js/createUser.js
+++ src/js/createUser.js
@@ -1,3 +1,5 @@
+const utenti = [];
+
 export function createUser() {
   const nome = document.getElementById("nome").value.trim();
   const cognome = document.getElementById("cognome").value.trim();
@@ -16,9 +18,53 @@ export function createUser() {
     indirizzo,
     cap,
     comune,
-    provincia
+    provincia,
   };
 
-  console.log(user);
-  
+  utenti.push(user);
+  console.log(utenti);
+}
+
+export function mostraUtenti() {
+  for (let i = 0; i < utenti.length; i++) {
+    const user = utenti[i];
+    const userEntry = document.createElement("div");
+    const details = document.createElement("div");
+    details.classList.add("user-details");
+    userEntry.classList.add("user-entry");
+
+    const header = document.createElement("div");
+    header.classList.add("user-header");
+
+    const title = document.createElement("span");
+    title.textContent = `${user.nome} ${user.cognome}`;
+
+    const toggleBtn = document.createElement("button");
+    toggleBtn.textContent = "+";
+
+    //dettagli Nascosti
+    details.innerHTML = `
+                            <p><strong>Età:</strong> ${user.eta}</p>
+                            <p><strong>Nazione:</strong> ${user.nazione}</p>
+                            <p><strong>Indirizzo:</strong> ${user.indirizzo}</p>
+                            <p><strong>CAP:</strong> ${user.cap}</p>
+                            <p><strong>Comune:</strong> ${user.comune}</p>
+                            <p><strong>Provincia:</strong> ${user.provincia}</p>`;
+
+    toggleBtn.addEventListener("click", () => {
+      if (details.classList.contains("show")) {
+        details.classList.remove("show");
+        toggleBtn.textContent = "+";
+      } else {
+        details.classList.add("show");
+        toggleBtn.textContent = "-";
+      }
+    });
+
+    header.appendChild(title);
+    header.appendChild(toggleBtn);
+    userEntry.appendChild(header);
+    userEntry.appendChild(details);
+    userList.appendChild(userEntry);
+  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,17 @@
-import { createUser } from "./createUser.js";
+import { createUser, mostraUtenti } from "./createUser.js";
 import { createPopShow } from "./allPop.js";
 import { resetForm } from "./allPop.js";
 import { closePop } from "./allPop.js";
 import { showSuccessToast } from "./allPop.js";
+
+
 const btnCreateUser = document.querySelector(".btn-primary");
 const popup = document.getElementById("userForm");
 const btnClosePopUser = document.getElementById("closeWindowCreateUser");
 const btnSaveUser = document.getElementById("saveUser");
+const btnChoseUser = document.querySelector(".btn-secondary");
+const popupSfoglia = document.getElementById("userBrowser");
+const btnCloseSfogliaUtente = document.getElementById("closeUserBrowser");
 
 btnCreateUser.addEventListener("click", () => createPopShow(popup));
 btnClosePopUser.addEventListener("click", () => {
@@ -19,3 +24,13 @@ btnSaveUser.addEventListener("click", () => {
   showSuccessToast();
   resetForm(userForm)
 });
+
+btnChoseUser.addEventListener("click", () => {
+    createPopShow(popupSfoglia);
+    mostraUtenti();
+})
+
+
+btnCloseSfogliaUtente.addEventListener("click", () => {
+    closePop(popupSfoglia);
+})
